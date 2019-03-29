15307caee0006e829fdd7be75b8e2f10f02ee5e6
yangchun <37919689+yangchunaijtt@users.noreply.github.com>
2019-03-29 16:06:14 +0800

    3.29

--- charteredBus/js/chartereBus.js
+++ charteredBus/js/chartereBus.js
@@ -316,8 +316,7 @@ $(function(){
         window.location.hash ="#selectcar";
     })
 
-// 定位
-    container.dwlocationg();
+
 // 监控路由
     window.onhashchange = hashChange;
     hashChange();
@@ -1614,27 +1613,28 @@ $(function(){
         }
     }
 // 地图操作的函数
-    var container = {
-        dwlocationg:function(){
-            // 定位功能  
-            AMap.plugin('AMap.Geolocation', function() {
-                var geolocation = new AMap.Geolocation({
-                    enableHighAccuracy:false, //是否使用高精度定位，默认:true
-                    timeout: 10000,          //超过10秒后停止定位，默认：5s
-                    buttonPosition:'RB',     //定位按钮的停靠位置
-                    buttonOffset: new AMap.Pixel(10, 20),//定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
-                    zoomToAccuracy: true  //定位成功后是否自动调整地图视野到定位点
-                });
-                map.addControl(geolocation);
-                geolocation.getCurrentPosition(function(status,result){
-                    if(status=='complete'){
-                        container.onsuccess(result);
-                    }else{
-                        console.log("定位失败",result);
-                    }
-                });
+
+// 定位功能  定位功能 
+        AMap.plugin('AMap.Geolocation', function() {
+            var geolocation = new AMap.Geolocation({
+                enableHighAccuracy:false, //是否使用高精度定位，默认:true
+                timeout: 10000,          //超过10秒后停止定位，默认：5s
+                buttonPosition:'RB',     //定位按钮的停靠位置
+                buttonOffset: new AMap.Pixel(10, 20),//定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
+                zoomToAccuracy: true  //定位成功后是否自动调整地图视野到定位点
             });
-        },
+            map.addControl(geolocation);
+            geolocation.getCurrentPosition(function(status,result){
+                if(status=='complete'){
+                    container.onsuccess(result);
+                }else{
+                    console.log("定位失败",result);
+                    showMessage1btn("定位失败,请刷新在试","",0);
+                }
+            });
+        });
+
+    var container = {
         onsuccess:function(result){   // 定位成功得到的数据
             console.log("定位成功",result);
             releaseData.dwlocationg = result;
